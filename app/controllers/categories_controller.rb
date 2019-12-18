bd11d44e8b1de40c6584827a7c74db5af478b794
DAICHI KUN <daichi@DAICHInoiMac.local>
2019-12-18 23:38:13 +0900

    Add category edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,49 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:show]
 
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 15).order('created_at DESC')
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully updated"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:error] = "Only admin can perform this action"
+      redirect_to root_path
+    end
+  end
+
 end
