5d1830f1a9ca53ca63ad4a69f666fe9030eb4278
DAICHI KUN <daichi@DAICHInoiMac.local>
2019-12-22 19:15:01 +0900

    update article form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def top
     redirect_to articles_path if logged_in?
+    @articles = Article.paginate(page: params[:page], per_page: 15).order('created_at DESC')
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@ class Article < ApplicationRecord
   has_many :categories, through: :article_categories
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 1500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 5000 }
   validates :user_id, presence: true
 end
