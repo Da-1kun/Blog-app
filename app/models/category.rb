a8f97ae411c8eb95a517d5b38730af036ddc6796
DAICHI KUN <daichi@DAICHInoiMac.local>
2020-05-07 10:53:29 +0900

    Adjust font-size

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,6 @@ class Category < ApplicationRecord
   has_many :articles, through: :article_categories
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates_uniqueness_of :name
+
+  default_scope -> { order(:name) }
 end
